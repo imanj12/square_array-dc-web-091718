8b62863ae62d14a0328627e2a1ea397dccc7318e
Iman Jawad <imanj12@gmail.com>
2018-09-05 22:04:44 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
   # your code here
+  array.each do number
+    new_number = 0
+    new_number = number * number
+    new_array = []
+    new_array << new_number
+  end
 end
